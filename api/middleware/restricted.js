45f18769bd4eb36df27f0d242b02873b5374af93
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-06-26 22:48:51 -0400

    try

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -28,7 +28,7 @@ module.exports = (req, res, next) => {
 function isValidToken(token) {
   try {
     // Verify and decode the token
-    const decoded = jwt.verify(token, "monkey");
+    const decoded = jwt.verify(token, token);
 
     // Perform additional checks or validations
     // For example, you could check the token expiration
