a011ee43458b6e407a6c9d3f3e46c120d68b2ad7
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-06-26 20:54:51 -0400

    login and register both work

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -56,7 +56,7 @@ router.post('/register', async (req, res) => {
   */
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   // res.end('implement login, please!');
 
 
@@ -69,20 +69,19 @@ router.post('/login', (req, res) => {
     })
   }
 
-  User.find(username)
-    .then(([user]) => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = buildToken(user)
+  const found = await User.find(username)
+    
+      if (found && bcrypt.compareSync(password, found.password)) {
+        const token = buildToken(req.body)
         res.status(200).json({
           message: `Welcome, ${username}`, token
         })
       }
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: "invalid credentials"
-      })
-    })
+      else{
+        res.status(500).json({message: "invalid credentials"})
+      }
+    
+    
 
 
 
