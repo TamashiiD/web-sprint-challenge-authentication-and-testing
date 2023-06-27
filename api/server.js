5caf61e60acc0a76eb01b2b13ed57abea29c2b5a
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-06-26 21:45:08 -0400

    trying to set up the session

--- api/server.js
+++ api/server.js
@@ -9,9 +9,22 @@ const jokesRouter = require('./jokes/jokes-router.js');
 
 const server = express();
 
+const session = require('express-session')
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use(session({
+    name: "monkey",
+    secret: "keep it secret",
+    cookie: {
+        maxAge: 1000 * 60,
+        secure: false,
+        httpOnly: false,
+    },
+    resave: false,
+    saveUninitialized: false,
+}))
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
